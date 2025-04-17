1d996bc022338ba8472fda571139766367bdbeff
pjsmelodev <pjsm.lesi@gmail.com>
2025-04-17 17:06:12 +0100

    Updated views; Created migrations

--- RSGymPT.Clients/Migrations/20250417155141_UpdateSchemaAfterAmountMove.Designer.cs
+++ RSGymPT.Clients/Migrations/20250417155141_UpdateSchemaAfterAmountMove.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RSGymPT.Clients.Data;
@@ -11,9 +12,11 @@ using RSGymPT.Clients.Data;
 namespace RSGymPT.Clients.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250417155141_UpdateSchemaAfterAmountMove")]
+    partial class UpdateSchemaAfterAmountMove
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RSGymPT.Clients/Migrations/20250417155141_UpdateSchemaAfterAmountMove.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RSGymPT.Clients.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSchemaAfterAmountMove : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Payments");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "PaymentTypes",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "PaymentTypes");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Payments",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
